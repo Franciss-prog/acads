feadacb55d3362d06058fc952f0e45799a134638
francissprog <francissprog@gmail.com>
2025-09-10 10:24:32 +0800

    push-to-repo

--- oop/circle.java
+++ oop/circle.java
@@ -1,25 +1,18 @@
 import java.util.Scanner;
 
 class Circle {
-
-void getArea(int radius, double PI) {
+void getArea(double radius, double PI) {
     double area = PI * Math.pow(radius, 2);
     System.out.println("Area of the circle: " + area);
 }
-
  public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     Circle circle = new Circle();
     final double PI = 3.14;
-
-
-    System.out.println("Enter radius of the circle:");
+    System.out.print("Enter radius of the circle:");
     double radius = scanner.nextDouble();
-
     circle.getArea(radius, PI);
-
-    scanner.close();
-    
+    scanner.close();    
     }  
 }
 
--- oop/hello.java
+++ oop/hello.java
@@ -1,7 +1,6 @@
 
 
-class Hello {
-    
+class Hello { 
     public static void main(String[] args) {
       System.out.println("My name is Franciss");
       System.out.println("My quote is \n\"Just do it\"");
