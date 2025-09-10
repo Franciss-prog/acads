dd2ea5f783adc63e162e3e50b67e75d6e992c678
francissprog <francissprog@gmail.com>
2025-09-11 07:49:58 +0800

    pracitce for lab act

---
+++ oop/adminPerms.java
@@ -0,0 +1,23 @@
+
+
+class adminPerms {
+    public static void main(String[] args) {
+    boolean isAdmin  = false;
+    boolean isLoggedIn = true;  
+    int securityLevel = 1;
+
+
+
+    if (isLoggedIn && (isAdmin || securityLevel <= 2)) {
+       System.out.println("Access granted");
+    }
+    else if (isAdmin) {
+        System.out.println("Overwrite access granted");
+    }
+    else {
+        System.out.println("Access denied");
+    }
+    }
+
+
+}
